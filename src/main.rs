b48d4140ef44da18cbff1215a2c0a668dda8fad9
Grakhovsky <grahovsky@gmail.com>
2022-09-13 07:21:16 +0300

    Enums

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 fn main() {
-    
+
+    // let get = Method::GET("abcd".to_string());
+    // let delete: Method = Method::DELETE(100);
+    // let post: Method = Method::POST;
+    // let put: Method = Method::PUT;
+
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
 
@@ -23,4 +28,24 @@ impl Server {
     pub fn run(&self) {
        println!("Listening on {}", self.addr);
     }
+}
+
+struct Request {
+    path: String,
+    query_string: String,
+    method: Method,
+}
+
+enum Method {
+    GET,
+    DELETE,
+    // GET(String),
+    // DELETE(u64),
+    POST,
+    PUT,
+    HEAD,
+    CONNECT,
+    OPTIONS,
+    TRACE,
+    PATCH,
 }
