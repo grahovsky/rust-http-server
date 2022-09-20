3b79e3f4fa2e45d470450b73eb112316570cf7c9
Grakhovsky <grahovsky@gmail.com>
2022-09-20 07:42:55 +0300

    the match expression

--- src/server.rs
+++ src/server.rs
@@ -17,7 +17,23 @@ impl Server {
         let listener = TcpListener::bind(&self.addr).unwrap();
 
         'outer: loop {
-            listener.accept();
+
+            match listener.accept() {
+                Ok((stream, addr)) => {
+                    let a = 5;
+                    println!("OK");
+                },
+                Err(e) => println!("Failed to establish a connection {}", e),
+                _ => {}
+            }
+
+            let res = listener.accept();
+
+            if res.is_err() {
+                continue;
+            }
+
+            let (stream, addr) = res.unwrap();
         }
 
         // let tup = (5, listener, "some");
